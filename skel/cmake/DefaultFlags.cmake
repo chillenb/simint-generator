61d452c9f0a133799b711eb15ea7e7f4b214794b
Christopher Hillenbrand <chillenbrand15@gmail.com>
2022-01-30 23:40:10 -0500

    openmp target offloading cmake flags

--- skel/cmake/DefaultFlags.cmake
+++ skel/cmake/DefaultFlags.cmake
@@ -60,6 +60,17 @@ else()
 
 endif()
 
+# You may need to pass in extra flags when using OpenMP target offloading
+# E.g. if you have clang and an NVIDIA Maxwell card, you might use the following flags:
+# -DSIMINT_OMP_TARGET_FLAGS="-fopenmp -fopenmp-targets=nvptx64 -Xopenmp-target -march=sm_50 --cuda-path=/opt/cuda"
+if(SIMINT_USE_OPENMP_TARGET)
+
+  list(APPEND SIMINT_C_FLAGS "${SIMINT_OMP_TARGET_FLAGS}")
+  list(APPEND SIMINT_CXX_FLAGS "${SIMINT_OMP_TARGET_FLAGS}")
+  list(APPEND SIMINT_LINK_FLAGS "${SIMINT_OMP_TARGET_FLAGS}")
+
+endif()
+
 # Check for unspecified or invalid vectorization type
 set(SIMINT_VALID_VECTOR
      scalar
