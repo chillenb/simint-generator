4397e1a66668cc780568ebd11555f04ed8630d93
Christopher Hillenbrand <chillenbrand15@gmail.com>
2022-04-11 13:54:52 -0400

    pass some const iterators by reference and make fast convenient build script for personal use

---
+++ convenient_build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -e "build" ]; then
+  rm -rf build
+fi
+mkdir build; cd build
+
+
+CC=gcc CXX=g++ cmake -DGENERATOR_CXX_FLAGS="-O3;-march=native;-flto" ../
+make -j16 VERBOSE=1
