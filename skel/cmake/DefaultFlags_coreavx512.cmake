eee3682ea445e7ca089df7f54029b139a8c2a4dc
Christopher Hillenbrand <chillenbrand15@gmail.com>
2025-11-10 18:59:16 -0500

    intel llvm reluctant to use avx512 svml

--- skel/cmake/DefaultFlags_coreavx512.cmake
+++ skel/cmake/DefaultFlags_coreavx512.cmake
@@ -1,10 +1,12 @@
-if("${CMAKE_C_COMPILER_ID}" MATCHES "Intel")
+if("${CMAKE_C_COMPILER_ID}" STREQUAL "Intel")
 
   list(APPEND SIMINT_C_FLAGS "-xCORE-AVX512;-fma")
   list(APPEND SIMINT_TESTS_CXX_FLAGS "-xCORE-AVX512;-fma")
 
 elseif("${CMAKE_C_COMPILER_ID}" MATCHES "GNU" OR
-       "${CMAKE_C_COMPILER_ID}" MATCHES "Clang")
+       "${CMAKE_C_COMPILER_ID}" MATCHES "Clang" OR
+       "${CMAKE_C_COMPILER_ID}" MATCHES "IntelLLVM")
+
 
   list(APPEND SIMINT_C_FLAGS "-mavx512f;-mavx512cd;-mavx512bw;-mavx512dq;-mavx512vl;-mfma")
   list(APPEND SIMINT_TESTS_CXX_FLAGS "-mavx512f;-mavx512cd;-mavx512bw;-mavx512dq;-mavx512vl;-mfma")
